d79c33ac8ed61949924f9019234320ef654d4c77
John <john.19col@gmail.com>
2018-04-30 22:28:28 -0500

    Add beta implementation of requestPokemonUsingChunks

--- src/app/services/pokemon-api.service.ts
+++ src/app/services/pokemon-api.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/observable/from';
 import 'rxjs/add/observable/fromPromise';
+import 'rxjs/add/observable/interval';
+
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/concatMap';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/bufferCount';
+import 'rxjs/add/operator/take';
 
 import * as Pokedex from 'pokeapi-js-wrapper';
 
@@ -14,7 +21,10 @@ const pokemonApiHttpOptions: any = {
   protocol: 'https',
   versionPath: '/api/v2/',
   cache: true
-}
+};
+
+const DEFAULT_CHUNK_SIZE: number = 3;
+const INTERVAL_BETWEEN_REQUESTS: number = 500;
 
 @Injectable()
 export class PokemonApiService {
@@ -50,6 +60,18 @@ export class PokemonApiService {
     const resources: string[] = identifiers.map(identifier => this.resourcePath + identifier);
     return Observable.fromPromise(this.api.resource(resources))
       .map(resource => this.mapper.mapAll(<PokemonResource[]>resource));
+  }
+
+  private requestPokemonUsingChunks(identifiers: any[], byChunksOf: number = DEFAULT_CHUNK_SIZE): Observable<Pokemon[]> {
+    return Observable.interval(INTERVAL_BETWEEN_REQUESTS)
+      .take(identifiers.length)
+      .map(i => identifiers[i])
+      .map(identifier => this.resourcePath + identifier)
+      .bufferCount(byChunksOf)
+      .map(resource => this.api.resource(resource))
+      .concatMap(resourcePromise => Observable.fromPromise(resourcePromise))
+      .map(pokemonResource => <PokemonResource[]>pokemonResource)
+      .map(pokemonResource => this.mapper.mapAll(pokemonResource));
   }
 
 }
